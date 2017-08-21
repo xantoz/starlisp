616e475954b88b253146e497f95ea15a4906d933
Anton Kindestam <antonki@kth.se>
2017-08-21 13:45:17 +0200

    JavaMethod.java: reindent nested ternaries for readability

--- src/main/java/org/starlisp/core/JavaMethod.java
+++ src/main/java/org/starlisp/core/JavaMethod.java
@@ -36,18 +36,19 @@ public final class JavaMethod extends LispFn {
   // TODO: For numbers (at least integers) only accept if a conversion won't lose precision. (in fact with this in place i
   // might just be able to, in argumentMoreSpecificThan, prefer narrower over wider for everything)
   private static boolean accept(Class javaArg, LispObject lispArg) {
-    return (javaArg == Object.class || LispObject.class.isAssignableFrom(javaArg)) ? true : // Move this line? (with or without the LispObject part?)
-        (javaArg == float.class || javaArg == Float.class ||
-            javaArg == double.class || javaArg == Double.class) ? (lispArg instanceof LispFlonum) :
-            (javaArg == char.class || javaArg == Character.class) ? (lispArg instanceof LispChar) :
-                (javaArg == short.class || javaArg == Short.class ||
-                    javaArg == int.class || javaArg == Integer.class ||
-                    javaArg == long.class || javaArg == Long.class ||
-                    javaArg == BigInteger.class) ? (lispArg instanceof LispInteger) :
-                    (javaArg == String.class) ? (lispArg instanceof LispString) :
-                        (javaArg == boolean.class || javaArg == Boolean.class) ? true :
-                            (lispArg instanceof JavaObject) ? javaArg.isInstance(((JavaObject) lispArg).getObj()) :
-                                false;
+    return
+      (javaArg == Object.class || LispObject.class.isAssignableFrom(javaArg)) ? true : // Move this line? (with or without the LispObject part?)
+      (javaArg == float.class || javaArg == Float.class ||
+       javaArg == double.class || javaArg == Double.class)                    ? (lispArg instanceof LispFlonum) :
+      (javaArg == char.class || javaArg == Character.class)                   ? (lispArg instanceof LispChar) :
+      (javaArg == short.class || javaArg == Short.class ||
+       javaArg == int.class || javaArg == Integer.class ||
+       javaArg == long.class || javaArg == Long.class ||
+       javaArg == BigInteger.class)                                           ? (lispArg instanceof LispInteger) :
+      (javaArg == String.class)                                               ? (lispArg instanceof LispString) :
+      (javaArg == boolean.class || javaArg == Boolean.class)                  ? true :
+      (lispArg instanceof JavaObject)                                         ? javaArg.isInstance(((JavaObject) lispArg).getObj()) :
+      false;
   }
 
   // Returns true if a is more specific than ("prefered" over) b, false otherwise. Additionally contains special case rules to
@@ -55,35 +56,34 @@ public final class JavaMethod extends LispFn {
   // float). TODO: Them arrays again
   // FIXME: bool support. think of it as a bool if it is either nil or t? special symbols javatrue javafalse?
   private static boolean argumentMoreSpecificThan(Class<?> a, Class<?> b) {
-    return (a == b) ? false : // Can't make a judgement now can we
-        // (a == Object.class)                        ? true : // BUGFISK? Object is always specificisestist
-        // Special: LispObject is more specific than anything else yo, also tricky rules
-        LispObject.class.isAssignableFrom(a) ? !(LispObject.class.isAssignableFrom(b) && a.isAssignableFrom(b)) :
-            //(a == boolean.class || a == Boolean.class) ? (b != boolean.class || b != Boolean.class || !LispObject.class.isAssignableFrom(b)) : // But lispobject wins over bools
-            (a == int.class || a == Integer.class) ? (b == short.class || b == Short.class) : // For numbers we prefer wider over narrower
-                (a == long.class || a == Long.class) ? (b == int.class || b == Integer.class || b == short.class || b == Short.class) :
-                    (a == BigInteger.class) ? (b == long.class || b == Long.class || b == int.class || b == Integer.class || b == short.class || b == Short.class) :
-                        (a == double.class || a == Double.class) ? (b == float.class || b == Float.class) :
-                            b.isAssignableFrom(a);                       // If b is assignable from a, a is more specific, "narrower", than b
+    return
+      (a == b)                                 ? false : // Can't make a judgement now can we
+      // Special: LispObject is more specific than anything else, also tricky rules
+      LispObject.class.isAssignableFrom(a)     ? !(LispObject.class.isAssignableFrom(b) && a.isAssignableFrom(b)) :
+      (a == int.class || a == Integer.class)   ? (b == short.class || b == Short.class) : // For numbers we prefer wider over narrower
+      (a == long.class || a == Long.class)     ? (b == int.class || b == Integer.class || b == short.class || b == Short.class) :
+      (a == BigInteger.class)                  ? (b == long.class || b == Long.class || b == int.class || b == Integer.class || b == short.class || b == Short.class) :
+      (a == double.class || a == Double.class) ? (b == float.class || b == Float.class) :
+      b.isAssignableFrom(a);                       // If b is assignable from a, a is more specific, "narrower", than b
   }
 
   // Conversion helpers, does the unboxing and boxing
   private static Object[] lispToJava(LispObject[] objs, Class[] argt) {
     Object[] res = new Object[objs.length];
     for (int i = 0; i < objs.length; ++i)
-      res[i] = // (objs[i] instanceof JavaObject)                         ? ((JavaObject)objs[i]).getObj() :
-          (LispObject.class.isAssignableFrom(argt[i])) ? objs[i] : // Wants a LispObject or subclass, no unboxing needed
-              (objs[i] instanceof JavaObject) ? ((JavaObject) objs[i]).getObj() :
-                  (argt[i] == float.class || argt[i] == Float.class) ? ((LispFlonum) objs[i]).toJavaFloat() :
-                      (argt[i] == double.class || argt[i] == Double.class) ? ((LispFlonum) objs[i]).toJavaDouble() :
-                          (argt[i] == int.class || argt[i] == Integer.class) ? ((LispInteger) objs[i]).toJavaInt() :
-                              (argt[i] == short.class || argt[i] == Short.class) ? new Short((short) ((LispInteger) objs[i]).toJavaInt()) :
-                                  (argt[i] == BigInteger.class) ? ((LispInteger) objs[i]).toJavaBigInteger() :
-                                      (argt[i] == char.class || argt[i] == Character.class) ? ((LispChar) objs[i]).ch() :
-                                          (argt[i] == long.class || argt[i] == Long.class) ? ((LispInteger) objs[i]).toJavaLong() :
-                                              (argt[i] == Boolean.class || argt[i] == boolean.class) ? objs[i] != null :
-                                                  (argt[i] == String.class) ? ((LispString) objs[i]).toJavaString() :
-                                                      objs[i];
+      res[i] =
+        (LispObject.class.isAssignableFrom(argt[i]))             ? objs[i] : // Wants a LispObject or subclass, no unboxing needed
+        (objs[i] instanceof JavaObject)                          ? ((JavaObject) objs[i]).getObj() :
+        (argt[i] == float.class   || argt[i] == Float.class)     ? ((LispFlonum) objs[i]).toJavaFloat() :
+        (argt[i] == double.class  || argt[i] == Double.class)    ? ((LispFlonum) objs[i]).toJavaDouble() :
+        (argt[i] == int.class     || argt[i] == Integer.class)   ? ((LispInteger) objs[i]).toJavaInt() :
+        (argt[i] == short.class   || argt[i] == Short.class)     ? new Short((short) ((LispInteger) objs[i]).toJavaInt()) :
+        (argt[i] == BigInteger.class)                            ? ((LispInteger) objs[i]).toJavaBigInteger() :
+        (argt[i] == char.class    || argt[i] == Character.class) ? ((LispChar) objs[i]).ch() :
+        (argt[i] == long.class    || argt[i] == Long.class)      ? ((LispInteger) objs[i]).toJavaLong() :
+        (argt[i] == Boolean.class || argt[i] == boolean.class)   ? objs[i] != null :
+        (argt[i] == String.class)                                ? ((LispString) objs[i]).toJavaString() :
+        objs[i];
     return res;
   }
 
@@ -169,27 +169,28 @@ public final class JavaMethod extends LispFn {
   }
 
   private LispObject javaToLisp(Object obj) {
-    return (obj == null) ? null :
-        (obj instanceof LispObject) ? (LispObject) obj :
-            (obj instanceof Float) ? new LispFlonum((Float) obj) :
-                (obj instanceof Double) ? new LispFlonum((Double) obj) :
-                    (obj instanceof Short) ? new LispFixnum((Short) obj) :
-                        (obj instanceof Integer) ? new LispFixnum((Integer) obj) :
-                            (obj instanceof Long) ? new LispFixnum((Long) obj) :
-                                (obj instanceof Character) ? LispChar.create((Character) obj) :
-                                    (obj instanceof BigInteger) ? new LispBigInt((BigInteger) obj) :
-                                        (obj instanceof String) ? new LispString((String) obj) :
-                                            (obj instanceof Boolean) ? (Boolean) obj == true ? Symbol$.MODULE$.t() : null :
-                                                new JavaObject(obj);
+    return
+      (obj == null)               ? null :
+      (obj instanceof LispObject) ? (LispObject) obj :
+      (obj instanceof Float)      ? new LispFlonum((Float) obj) :
+      (obj instanceof Double)     ? new LispFlonum((Double) obj) :
+      (obj instanceof Short)      ? new LispFixnum((Short) obj) :
+      (obj instanceof Integer)    ? new LispFixnum((Integer) obj) :
+      (obj instanceof Long)       ? new LispFixnum((Long) obj) :
+      (obj instanceof Character)  ? LispChar.create((Character) obj) :
+      (obj instanceof BigInteger) ? new LispBigInt((BigInteger) obj) :
+      (obj instanceof String)     ? new LispString((String) obj) :
+      (obj instanceof Boolean)    ? (Boolean) obj == true ? Symbol$.MODULE$.t() : null :
+      new JavaObject(obj);
   }
 
   // Gives a list of classes; the argument types of the list, used as a key for memoizing in methodMap
   private List<Class> getArgumentTypes(Object[] list) {
     List<Class> res = new ArrayList<Class>(list.length + 1);
     for (int i = 0; i < list.length; ++i)
-      res.add((list[i] == null) ? null : // Don't want NullPointerExceptions
-                  (list[i] instanceof JavaObject) ? ((JavaObject) list[i]).getObj().getClass() : // For JavaObject it is more useful to use the class of the object it contains
-                      list[i].getClass());
+      res.add((list[i] == null)               ? null : // Don't want NullPointerExceptions
+              (list[i] instanceof JavaObject) ? ((JavaObject) list[i]).getObj().getClass() : // For JavaObject it is more useful to use the class of the object it contains
+              list[i].getClass());
     return res;
   }
 
@@ -198,7 +199,7 @@ public final class JavaMethod extends LispFn {
     try {
       List<Class> argumentTypes = getArgumentTypes(objects);
       Class storeKlas = (obj instanceof Class) ? (Class) obj : obj.getClass(); // We need to use obj as key when it is an instance of Class
-      argumentTypes.add(0, storeKlas);                                        // prepend the class to the list of argument types
+      argumentTypes.add(0, storeKlas);        // prepend the class to the list of argument types
       Monstructor method = methodMap.get(name()).get(argumentTypes);
       if (method == null) {
         int m = matchMethod(objects);
